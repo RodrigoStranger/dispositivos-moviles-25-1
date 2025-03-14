e2e22426745b287420b20f81cf15db52a292ee99
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-14 09:59:45 -0500

    Agregabdo menu

--- Practica 1 - Introduccion a Kotlin/Calculadora elemental.kt
+++ Practica 1 - Introduccion a Kotlin/Calculadora elemental.kt
@@ -1,2 +1,21 @@
 package `Practica 1 - Introduccion a Kotlin`
 
+/*
+    Autor: Rodrigo Emerson Infanzon Acosta
+    Curso: Programacion De Dispositivos Moviles
+    Semestre: VI
+    Fecha: 14/03/2025
+    Ultima modificacion: 14/03/2025 10.00 am
+*/
+
+// Función para mostrar el menú
+fun mostrarMenu() {
+    println("\n==== Menú ====")
+    println("1. Suma")
+    println("2. Resta")
+    println("3. Multiplicación")
+    println("4. División")
+    println("5. Salir")
+    print("Seleccione una opción: ")
+}
+

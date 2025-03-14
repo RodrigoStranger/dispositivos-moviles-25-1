df3b2b28c24feb27f1c9581c152b2742222b109a
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-14 10:20:10 -0500

    Agregando calculadora

--- Practica 1 - Introduccion a Kotlin/Calculadora elemental.kt
+++ Practica 1 - Introduccion a Kotlin/Calculadora elemental.kt
@@ -19,3 +19,20 @@ fun mostrarMenu() {
     print("Seleccione una opción: ")
 }
 
+
+// Funciones de operaciones matemáticas
+fun suma(a: Int, b: Int) = a + b
+fun resta(a: Int, b: Int) = a - b
+fun multiplicacion(a: Int, b: Int) = a * b
+fun division(a: Int, b: Int): String = if (b != 0) (a / b).toString() else "No se puede dividir entre 0"
+
+fun calculadora(opcion: Int, numero1: Int, numero2: Int): String {
+    return when (opcion) {
+        1 -> "Resultado: ${suma(numero1, numero2)}"
+        2 -> "Resultado: ${resta(numero1, numero2)}"
+        3 -> "Resultado: ${multiplicacion(numero1, numero2)}"
+        4 -> "Resultado: ${division(numero1, numero2)}"
+        5 -> "Saliendo de la calculadora..."
+        else -> "Error: opción no reconocida"
+    }
+}

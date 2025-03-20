c559d948759f20272dbe9e969ef1382b4c667fc1
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-20 14:05:33 -0500

    Agregando la clase CuentaBancaria, incluyendo sus atributos y metodos

--- Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Cuenta Bancaria.kt
+++ Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Cuenta Bancaria.kt
@@ -5,7 +5,7 @@ package `Practica 2 - Programacion Orientada a Objetos Kotlin`
     Curso: Programacion De Dispositivos Moviles
     Semestre: VI
     Fecha: 20/03/2025
-    Ultima modificacion: 20/03/2025 13:55 pm
+    Ultima modificacion: 20/03/2025 14:05 pm
 */
 
 /*
@@ -15,3 +15,47 @@ package `Practica 2 - Programacion Orientada a Objetos Kotlin`
     Utilice el set para validar datos.
 */
 
+class CuentaBancaria(private var saldo: Number, private var limiteRetiro: Number) {
+
+    // Obtener el saldo actual
+    fun getSaldo(): Number { return saldo }
+
+    // Modificar el saldo
+    fun setSaldo(nuevoSaldo: Number) {
+        if (nuevoSaldo.toDouble() >= 0) {
+            saldo = nuevoSaldo
+        } else {
+            println("No puede existir un saldo negativo")
+        }
+    }
+
+    // Obtener el limite de un retiro
+    fun getLimiteRetiro(): Number {
+        return limiteRetiro
+    }
+
+    // Modificar el limite de un retiro
+    fun setLimiteRetiro(nuevoLimite: Number) {
+        if (nuevoLimite.toDouble() > 0) {
+            limiteRetiro = nuevoLimite
+        } else {
+            println("El limite de retiro debe ser mayor a 0")
+        }
+    }
+
+    // Metodo para realizar un retiro
+    fun retirar(monto: Number) {
+        val montoDouble = monto.toDouble()
+        val saldoDouble = saldo.toDouble()
+        val limiteDouble = limiteRetiro.toDouble()
+
+        when {
+            montoDouble > saldoDouble -> println("Error: Saldo insuficiente.")
+            montoDouble > limiteDouble -> println("Error: El monto excede el límite de retiro.")
+            else -> {
+                saldo = saldoDouble - montoDouble
+                println("Retiro exitoso. Nuevo saldo: $saldo")
+            }
+        }
+    }
+}

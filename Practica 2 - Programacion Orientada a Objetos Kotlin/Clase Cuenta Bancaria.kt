169324e5240bc78ca56aa9c551b7d697ae3dfd23
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-20 13:55:45 -0500

    Agregando descripcion y autor al codigo

--- Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Cuenta Bancaria.kt
+++ Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Cuenta Bancaria.kt
@@ -1,2 +1,17 @@
 package `Practica 2 - Programacion Orientada a Objetos Kotlin`
 
+/*
+    Autor: Rodrigo Emerson Infanzon Acosta
+    Curso: Programacion De Dispositivos Moviles
+    Semestre: VI
+    Fecha: 20/03/2025
+    Ultima modificacion: 20/03/2025 13:55 pm
+*/
+
+/*
+    Diseña una clase CuentaBancaria que tenga un saldo y un límite de retiro.
+    Implementa métodos set y get para establecer y obtener el saldo,
+    y añade un metodo para realizar retiros que tenga en cuenta el límite de retiro.
+    Utilice el set para validar datos.
+*/
+

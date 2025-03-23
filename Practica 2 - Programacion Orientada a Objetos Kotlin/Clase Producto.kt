df92597e6ad0f7a7220ecdc0648651738618e378
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-22 23:22:50 -0500

    Agregando cabecera

--- Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Producto.kt
+++ Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Producto.kt
@@ -1,2 +1,17 @@
 package `Practica 2 - Programacion Orientada a Objetos Kotlin`
 
+/*
+    Autor: Rodrigo Emerson Infanzon Acosta
+    Curso: Programacion De Dispositivos Moviles
+    Semestre: VI
+    Fecha: 22/03/2025
+    Ultima modificacion: 22/03/2025 23:21 pm
+*/
+
+/*
+    Diseña una clase Producto que tenga un precio y un descuento aplicable.
+    Implementa métodos set y get para establecer y obtener el precio y el descuento,
+    y añade un metodo para calcular el precio final después de aplicar el descuento.
+    Utilice el set para validar datos.
+*/
+

71763df1cdfa34c7cf5dcbbca4ad01f7fe8ead89
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-22 22:34:01 -0500

    Poniendo comentarios

--- Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Figuras.kt
+++ Practica 2 - Programacion Orientada a Objetos Kotlin/Clase Figuras.kt
@@ -1,2 +1,9 @@
 package `Practica 2 - Programacion Orientada a Objetos Kotlin`
 
+/*
+    Autor: Rodrigo Emerson Infanzon Acosta
+    Curso: Programacion De Dispositivos Moviles
+    Semestre: VI
+    Fecha: 20/03/2025
+    Ultima modificacion: 22/03/2025 22:33 pm
+*/

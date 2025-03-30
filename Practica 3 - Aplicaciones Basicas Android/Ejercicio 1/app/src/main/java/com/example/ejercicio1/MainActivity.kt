2e31b7c40223e470bf1e77f0fd9c6f173b4927fc
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-30 00:31:17 -0500

    Moví la llamada a stringResource fuera del callback del clickable y la guardé en una variable toastMessage

--- Practica 3 - Aplicaciones Basicas Android/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Practica 3 - Aplicaciones Basicas Android/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -23,27 +23,23 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Ejercicio1Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                MainScreen()
             }
         }
     }
@@ -52,6 +48,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val context = LocalContext.current
+    val toastMessage = stringResource(id = R.string.toast_message)
+    
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -64,7 +62,7 @@ fun MainScreen() {
                 .clickable {
                     Toast.makeText(
                         context,
-                        "¡Has hecho clic en la imagen!",
+                        toastMessage,
                         Toast.LENGTH_SHORT
                     ).show()
                 }

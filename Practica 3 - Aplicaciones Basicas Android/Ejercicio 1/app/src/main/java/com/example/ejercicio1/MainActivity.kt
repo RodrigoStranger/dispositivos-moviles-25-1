778e294a37b76483f3b21784659ba300185f1ac5
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-03-30 00:26:16 -0500

    agregando cabeceras

--- Practica 3 - Aplicaciones Basicas Android/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Practica 3 - Aplicaciones Basicas Android/Ejercicio 1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -1,29 +1,48 @@
 package com.example.ejercicio1
 
+/*
+    Autor: Rodrigo Emerson Infanzon Acosta
+    Curso: Programacion De Dispositivos Moviles
+    Semestre: VI
+    Fecha: 14/03/2025
+    Ultima modificacion: 30/03/2025 0:30 am
+*/
+
+/*
+    Agregar una imagen a tu actividad.
+    Programar la funcionalidad para que al hacer clic en la imagen, se muestre un Toast con un mensaje personalizado.
+
+*/
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Ejercicio1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
                 }
             }
         }
@@ -31,17 +50,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ejercicio1Theme {
-        Greeting("Android")
+fun MainScreen() {
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Imagen clickeable",
+            modifier = Modifier
+                .size(200.dp)
+                .clickable {
+                    Toast.makeText(
+                        context,
+                        "¡Has hecho clic en la imagen!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        )
     }
 }

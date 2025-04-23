5bd12e9f03a21f8e11d165fc9067ac05e7b2ebaa
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-04-23 11:15:17 -0500

    cabecera del proyecyo, autor dia fecha etc

--- Practica 5 - Fragments y Navegacion/Configuradordepedidosdecomida/app/src/main/java/com/example/configuradordepedidosdecomida/MainActivity.kt
+++ Practica 5 - Fragments y Navegacion/Configuradordepedidosdecomida/app/src/main/java/com/example/configuradordepedidosdecomida/MainActivity.kt
@@ -1,5 +1,42 @@
 package com.example.configuradordepedidosdecomida
 
+/*
+    Autor: Rodrigo Emerson Infanzon Acosta
+    Curso: Programacion De Dispositivos Moviles
+    Semestre: VI
+    Fecha: 19/03/2025
+    Ultima modificacion: 23/03/2025 11:15 am
+*/
+
+/*
+    Configurador de pedido de comida.
+    Objetivo: Permitir al usuario armar un pedido paso a paso y visualizarlo al final.
+
+    Fragments:
+    InicioFragment
+        - Botón: "Nuevo pedido" → navega a SeleccionComidaFragment.
+
+    SeleccionComidaFragment
+        - Selección de comida (ej: pizza, hamburguesa, ensalada).
+        - Al hacer clic en “Siguiente”: Se crea un Bundle con el tipo de comida seleccionado.
+        - Se navega a SeleccionExtrasFragment.
+
+    SeleccionExtrasFragment
+        - Selección de extras (bebida, papas, postre).
+        - Al hacer clic en “Siguiente”: Se agrega la info de los extras al mismo Bundle o se
+          crea uno nuevo con todos los datos.
+        - Se navega a ResumenPedidoFragment.
+
+    ResumenPedidoFragment
+    - Usa arguments?.getString(...), etc. para mostrar la comida y extras.
+    - Botones:
+        - “Confirmar pedido”: muestra un Toast y vuelve al inicio (navigate()).
+        - "Editar pedido”:
+        - Usa setFragmentResult() con los datos actuales del pedido.
+        - Llama a popBackStack() para regresar a SeleccionComidaFragment,
+          que los puede recuperar con setFragmentResultListener(...).
+*/
+
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
